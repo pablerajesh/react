92a7c1d2e31a1ca0a774b59565220747789beaa6
RAJESH PABLE <pablerajesh@gmail.com>
2024-12-05 21:38:53 +0530

    Use new split button component

--- ag-grid-app/src/App.tsx
+++ ag-grid-app/src/App.tsx
@@ -2,10 +2,10 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import AccountGrid from "./ag-grid-tree-account/AccountGrid";
+import CustomerMapping from "./customer-mapping-tree";
 
 const App = () => {
-  return <AccountGrid />;
+  return <CustomerMapping />;
 };
 
 export default App;
--- ag-grid-app/src/customer-mapping-tree/index.tsx
+++ ag-grid-app/src/customer-mapping-tree/index.tsx
@@ -1,6 +1,7 @@
 import { Box, Button } from "@mui/material";
 import Grid from "@mui/material/Grid2";
 import { useEffect, useState } from "react";
+import SplitButton, { IButtonSpec } from "../components/mui/SplitButton";
 import {
   getCustomerHierarchies,
   getOrphanCustomersFromBackend
@@ -9,7 +10,7 @@ import {
   ICustomer,
   ICustomerHierarchy
 } from "../customer-mapping-common/types.def";
-import SplitButton from "./components/SplitButton";
+
 import CustomersWithParentsDisplay from "./customers-with-parents";
 import OrphanCustomersDisplay from "./orphan-customers";
 
@@ -19,7 +20,25 @@ const CustomerMapping = () => {
     >([]),
     [orphanCustomers, setOrphanCustomers] = useState<ICustomer[] | undefined>(
       undefined
-    );
+    ),
+    buttonSpecs: IButtonSpec[] = [
+      {
+        id: 1,
+        text: "Add selected to existing parent",
+        disabled: false,
+        onClick: () => {
+          console.log("[rp] Adding selected to existing parent");
+        }
+      },
+      {
+        id: 2,
+        text: "Add selected to a new parent",
+        disabled: false,
+        onClick: () => {
+          console.log("[rp] Adding selected to a new parent");
+        }
+      }
+    ];
 
   useEffect(() => {
     getCustomerHierarchies().then(customerHierarchies => {
@@ -42,13 +61,20 @@ const CustomerMapping = () => {
         <Grid size={6} border={1} borderColor={"#e0e0e0"}>
           <OrphanCustomersDisplay orphanCustomers={orphanCustomers} />
         </Grid>
-        <Grid size={6} container justifyContent={"flex-end"}>
-          <Button fullWidth size="large" variant="outlined">
+        <Grid size={6} container justifyContent={"flex-start"}>
+          <Button size="large" variant="outlined">
             Remove selected children
           </Button>
         </Grid>
-        <Grid size={6} container justifyContent={"flex-start"}>
-          <SplitButton />
+        <Grid size={6} container justifyContent={"flex-end"}>
+          <SplitButton
+            props={{
+              variant: "contained",
+              color: "primary",
+              size: "large"
+            }}
+            buttonSpecs={buttonSpecs}
+          />
         </Grid>
       </Grid>
     </Box>
