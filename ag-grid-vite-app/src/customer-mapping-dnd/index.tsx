241f723a4bbff97c21204ebc8d6c12d2b713562a
RAJESH PABLE <pablerajesh@gmail.com>
2024-12-25 20:42:00 +0530

    Configure prettier

--- ag-grid-vite-app/src/customer-mapping-dnd/index.tsx
+++ ag-grid-vite-app/src/customer-mapping-dnd/index.tsx
@@ -3,37 +3,56 @@ import Grid from "@mui/material/Grid2";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-material.css";
 import { useEffect, useState } from "react";
-import { getCustomerHierarchies, getOrphanCustomersFromBackend } from "../common/service";
+import {
+    getCustomerHierarchies,
+    getOrphanCustomersFromBackend
+} from "../common/service";
 import { ICustomer, ICustomerHierarchy } from "../common/types.def";
 import CustomersWithParentsDisplay from "./customers-with-parents";
 import OrphanCustomersDisplay from "./orphan-customers";
 
 const CustomerMappingDnd = () => {
-  const [, setCustomerHierarchies] = useState<ICustomerHierarchy[] | undefined>([]);
-  const [orphanCustomers, setOrphanCustomers] = useState<ICustomer[] | undefined>(undefined);
+    const [, setCustomerHierarchies] = useState<
+        ICustomerHierarchy[] | undefined
+    >([]);
+    const [orphanCustomers, setOrphanCustomers] = useState<
+        ICustomer[] | undefined
+    >(undefined);
 
-  useEffect(() => {
-    getCustomerHierarchies().then(customerHierarchies => {
-      setCustomerHierarchies(customerHierarchies);
-    });
+    useEffect(() => {
+        getCustomerHierarchies().then(customerHierarchies => {
+            setCustomerHierarchies(customerHierarchies);
+        });
 
-    getOrphanCustomersFromBackend().then(orphanCustomers => {
-      setOrphanCustomers(orphanCustomers);
-    });
-  }, []);
+        getOrphanCustomersFromBackend().then(orphanCustomers => {
+            setOrphanCustomers(orphanCustomers);
+        });
+    }, []);
 
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2} sx={{ p: 2 }}>
-        <Grid size={6} border={1} borderColor={"#e0e0e0"}>
-          <CustomersWithParentsDisplay />
-        </Grid>
-        <Grid size={6} border={1} borderColor={"#e0e0e0"}>
-          <OrphanCustomersDisplay orphanCustomers={orphanCustomers} />
-        </Grid>
-      </Grid>
-    </Box>
-  );
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid
+                container
+                spacing={2}
+                sx={{ p: 2 }}
+            >
+                <Grid
+                    size={6}
+                    border={1}
+                    borderColor={"#e0e0e0"}
+                >
+                    <CustomersWithParentsDisplay />
+                </Grid>
+                <Grid
+                    size={6}
+                    border={1}
+                    borderColor={"#e0e0e0"}
+                >
+                    <OrphanCustomersDisplay orphanCustomers={orphanCustomers} />
+                </Grid>
+            </Grid>
+        </Box>
+    );
 };
 
 export default CustomerMappingDnd;
--- ag-grid-vite-app/src/customer-mapping-dnd/orphan-customers/index.tsx
+++ ag-grid-vite-app/src/customer-mapping-dnd/orphan-customers/index.tsx
@@ -2,44 +2,55 @@ import { Container, Typography } from "@mui/material";
 import { ColDef } from "ag-grid-community";
 import { AgGridReact } from "ag-grid-react";
 import { useEffect, useMemo, useState } from "react";
-import { customerColDefs, customerDefaultCollDef, ICustomer } from "../../common/types.def";
+import {
+    customerColDefs,
+    customerDefaultCollDef,
+    ICustomer
+} from "../../common/types.def";
 
 export interface IOrphanCustomersDisplayProps {
-  orphanCustomers: ICustomer[] | undefined;
+    orphanCustomers: ICustomer[] | undefined;
 }
 
-export const OrphanCustomersDisplay = ({ orphanCustomers }: IOrphanCustomersDisplayProps) => {
-  const defaultColDef = useMemo<ColDef<ICustomer>>(() => customerDefaultCollDef, []);
-  const [columnDefs] = useState<ColDef<ICustomer>[]>(customerColDefs);
-  const [rowData, setRowData] = useState<ICustomer[] | undefined>(orphanCustomers);
+export const OrphanCustomersDisplay = ({
+    orphanCustomers
+}: IOrphanCustomersDisplayProps) => {
+    const defaultColDef = useMemo<ColDef<ICustomer>>(
+        () => customerDefaultCollDef,
+        []
+    );
+    const [columnDefs] = useState<ColDef<ICustomer>[]>(customerColDefs);
+    const [rowData, setRowData] = useState<ICustomer[] | undefined>(
+        orphanCustomers
+    );
 
-  useEffect(() => {
-    setRowData(orphanCustomers);
-  }, [orphanCustomers]);
+    useEffect(() => {
+        setRowData(orphanCustomers);
+    }, [orphanCustomers]);
 
-  return (
-    <Container sx={{ mt: 2 }}>
-      <Typography variant="h5">ORPHAN CUSTOMERS</Typography>
-      <div
-        id="orphan-customers-grid-container"
-        className={"ag-theme-material"}
-        style={{
-          height: "90vh",
-          minHeight: "90vh",
-          width: "100%"
-        }}
-      >
-        <AgGridReact
-          gridId="orphan-customers-grid"
-          rowData={rowData}
-          columnDefs={columnDefs}
-          defaultColDef={defaultColDef}
-          rowSelection={"multiple"}
-          overlayNoRowsTemplate="No orphan customers to display..."
-        />
-      </div>
-    </Container>
-  );
+    return (
+        <Container sx={{ mt: 2 }}>
+            <Typography variant="h5">ORPHAN CUSTOMERS</Typography>
+            <div
+                id="orphan-customers-grid-container"
+                className={"ag-theme-material"}
+                style={{
+                    height: "90vh",
+                    minHeight: "90vh",
+                    width: "100%"
+                }}
+            >
+                <AgGridReact
+                    gridId="orphan-customers-grid"
+                    rowData={rowData}
+                    columnDefs={columnDefs}
+                    defaultColDef={defaultColDef}
+                    rowSelection={"multiple"}
+                    overlayNoRowsTemplate="No orphan customers to display..."
+                />
+            </div>
+        </Container>
+    );
 };
 
 export default OrphanCustomersDisplay;
